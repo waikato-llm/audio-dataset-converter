2554779690ef1cb77fef29911e358b9c096819f6
Peter Reutemann <fracpete@gmail.com>
2024-06-27 16:59:48 +1200

    save_audio now checks whether self._source is not None before checking against path

--- src/adc/api/_data.py
+++ src/adc/api/_data.py
@@ -284,9 +284,10 @@ class AudioData(MetaDataHandler, LoggingHandler):
         :return: whether the file was saved
         :rtype: bool
         """
-        if os.path.exists(path) and os.path.exists(self._source) and os.path.samefile(path, self._source):
-            self.logger().warning("Input/output audio file are the same, skipping!")
-            return False
+        if self._source is not None:
+            if os.path.exists(path) and os.path.exists(self._source) and os.path.samefile(path, self._source):
+                self.logger().warning("Input/output audio file are the same, skipping!")
+                return False
         if make_dirs:
             parent_dir = os.path.dirname(path)
             if not os.path.exists(parent_dir):
