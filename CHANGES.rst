bda750456df07d96187fa3d945b8e6fc0f34db06
Peter Reutemann <fracpete@gmail.com>
2025-07-10 20:54:02 +1200

    prepared for release

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.0.3 (????-??-??)
+0.0.3 (2025-07-10)
 ------------------
 
 - added `set-placeholder` filter for dynamically setting (temporary) placeholders at runtime
--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "soundfile",
         "tinytag",
     ],
-    version="0.0.2",
+    version="0.0.3",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
